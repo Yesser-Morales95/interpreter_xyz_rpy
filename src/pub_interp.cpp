5aa4b382da3406b64764cca37103e470c357ba61
Yesser-Morales95 <moralesalfredo133@gmail.com>
2018-10-30 13:47:34 -0600

    Interpreter Node Buffer 10

--- src/pub_interp.cpp
+++ src/pub_interp.cpp
@@ -2,31 +2,28 @@
 #include "std_msgs/String.h"
 #include <trajectory_msgs/JointTrajectory.h>
 #include <trajectory_msgs/JointTrajectoryPoint.h>
-
 #include <boost/thread/thread.hpp>
 #include <boost/bind.hpp>
+#include <vector>
+#include <array>
 
-
-
-//double returnValue()
-//{
-//  return joint;
-////  jointz = susc;
-//}
 
 class SubscriberAndPublish {
 public:
   SubscriberAndPublish(){
-    joint_sub = nh.subscribe("/set_joint_trajectory",1,&SubscriberAndPublish::trajectoryCallback, this);
-    joint_pub = nh.advertise<trajectory_msgs::JointTrajectory>("set_joint_trajectory_delay", 1);
+    joint_sub = nh.subscribe("/set_joint_trajectory",10,&SubscriberAndPublish::trajectoryCallback, this);
+    joint_pub = nh.advertise<trajectory_msgs::JointTrajectory>("set_joint_trajectory_delay", 10);
     while(ros::ok())
        {
            //ROS_WARN("In Loop.");
            ros::spin();
        }
+
   }
+
   void trajectoryCallback(const trajectory_msgs::JointTrajectory &msg);
   trajectory_msgs::JointTrajectory msg1;
+  std::vector<double> jointvaluesOLD = {0.0,0.0,0.0,0.0,0.0,0.0};
 private:
     ros::Subscriber joint_sub;
     ros::Publisher joint_pub;
@@ -38,26 +35,39 @@ private:
 //  joint_sub = nh.subscribe("/set_joint_trajectory",1,&SubscriberAndPublish::trajectoryCallback, this);
 //  joint_pub = nh.advertise<trajectory_msgs::JointTrajectory>("set_joint_trajectory_delay", 1);
 //}
-void SubscriberAndPublish::trajectoryCallback(const trajectory_msgs::JointTrajectory &msg)
+void SubscriberAndPublish::trajectoryCallback(const trajectory_msgs::JointTrajectory &msg) // toma el ultimo valor
 {
+//                 jointvaluesOLD(6);
+                  msg1.points.resize(1);
+                  msg1.points[0].positions.resize(6);
                   double jointx, joint;
+                  std::vector<double> jointvalues(6);
+                  std::vector<double> jointvaluesminus(6);
                   std::cout <<msg<<std::endl;
 
-                  jointx = msg.points[0].positions[0];
-                  for (double i = 0; i<20; i++){
+                  for (int i = 0; i < 6; i++) {
+                    jointvalues[i] = msg.points[0].positions[i];
+                    jointvaluesminus[i] = jointvaluesOLD[i] - jointvalues[i];
+                  }
 
-                  joint= (i/20)*jointx;
-                  boost::this_thread::sleep(boost::posix_time::milliseconds(100));
-                  std::cout <<joint<<std::endl;
-                  msg1.points.resize(1);
-                  msg1.points[0].positions.resize(1);
-//                  msg1.points[0].positions[0].push_back(joint); //array [i]
-                  msg1.points[0].positions[0]=joint;
+                  for (double j = 0; j<=20; j++){
+
+//                  joint= (i/20)*jointx;
+                  boost::this_thread::sleep(boost::posix_time::milliseconds(200));
+//                  std::cout <<jointvalues<<std::endl;
+                 for (int k = 0; k < 6; k++) {
+                  msg1.points[0].positions[k] =jointvaluesOLD[k] + (((-1)*jointvaluesminus[k])*(j/20)); //array [i]
+
+                 }
                   msg1.points[0].time_from_start = ros::Duration(0.1);
                   msg1.header.frame_id = "my_lab_uni_delay";
 //                  msg1.header.stamp = ros::Time::now();
                   joint_pub.publish(msg1) ;
                     }//end FOR
+
+                   for (int l = 0; l < 6; l++) {
+                  jointvaluesOLD[l]=jointvalues[l];
+                   }
 
 }
 
@@ -66,20 +76,5 @@ int main(int argc, char **argv)
 {
   ros::init(argc, argv, "pub_interp");
   SubscriberAndPublish SaPObject;
-//  ros::Rate	loop_rate(10);
-
-//      while	(ros::ok())
-//        {
-////        joint=joint + (jointx/10);
-////             do{
-////SaPObject.joint_sub;
-//                  ros::spinOnce();
-//                  loop_rate.sleep();
-////                  flag++;
-////                  }  while(flag!=20);
-
-//      } //end While
-
-
   return 0;
 }
