3d52f98a5244a85cddefc7c80ae119676937da49
Yesser-Morales95 <moralesalfredo133@gmail.com>
2019-01-03 19:19:52 -0600

    add points and velocities, delay

--- src/pub_interp.cpp
+++ src/pub_interp.cpp
@@ -12,8 +12,12 @@
 class SubscriberAndPublish {
 public:
   SubscriberAndPublish(){
-    joint_sub = nh.subscribe("/set_joint_trajectory",10,&SubscriberAndPublish::trajectoryCallback, this);
+    joint_sub = nh.subscribe("/set_joint_trajectory",1,&SubscriberAndPublish::trajectoryCallback, this);
     joint_pub = nh.advertise<trajectory_msgs::JointTrajectory>("set_joint_trajectory_delay", 10);
+//    joint_pub_sub = nh.advertise<trajectory_msgs::JointTrajectory>("set_joint_trajectory", 10);
+
+    msgpub.points.resize(1);
+    msgpub.points[0].positions.resize(6);
     while(ros::ok())
        {
            //ROS_WARN("In Loop.");
@@ -24,11 +28,12 @@ public:
 
   void trajectoryCallback(const trajectory_msgs::JointTrajectory &msg);
 
-  trajectory_msgs::JointTrajectory msg1;
+  trajectory_msgs::JointTrajectory msg1, msgpub;
   std::vector<double> jointvaluesOLD = {0.0,0.0,0.0,0.0,0.0,0.0};
 private:
     ros::Subscriber joint_sub;
     ros::Publisher joint_pub;
+//    ros::Publisher joint_pub_sub;
     ros::NodeHandle nh;
 
 };
@@ -40,26 +45,33 @@ private:
 
 void SubscriberAndPublish::trajectoryCallback(const trajectory_msgs::JointTrajectory &msg) // toma el ultimo valor
 {
+
 //                 jointvaluesOLD(6);
+                  int it=0;
                   msg1.points.resize(1);
                   msg1.points[0].positions.resize(6);
                   double jointx, joint;
                   std::vector<double> jointvalues(6);
+                  std::vector<double> velvalues(6);
                   std::vector<double> jointvaluesminus(6);
                   std::cout <<msg<<std::endl;
+                  it = msg.points.size();
+//                  msg.joint_names.pu
+                  for (int m=1; m<it; m++){
 
                   for (int i = 0; i < 6; i++) {
-                    jointvalues[i] = msg.points[0].positions[i];
+                    jointvalues[i] = msg.points[m].positions[i];
+                    velvalues  [i] = msg.points[m].velocities[i];
+//                    msgpub.points[0].positions[i] = msg.points[0].positions[i];
                     jointvaluesminus[i] = jointvaluesOLD[i] - jointvalues[i];
                   }
 
-                  for (double j = 0; j<=20; j++){
 
+                  for (double j = 0; j<=20; j++){
 //                  joint= (i/20)*jointx;
-                  boost::this_thread::sleep(boost::posix_time::milliseconds(200));
-//                  std::cout <<jointvalues<<std::endl;
                  for (int k = 0; k < 6; k++) {
                   msg1.points[0].positions[k] =jointvaluesOLD[k] + (((-1)*jointvaluesminus[k])*(j/20)); //array [i]
+                  boost::this_thread::sleep(boost::posix_time::milliseconds(101 - velvalues  [k]));
 
                  }
                   msg1.points[0].time_from_start = ros::Duration(0.1);
@@ -71,6 +83,20 @@ void SubscriberAndPublish::trajectoryCallback(const trajectory_msgs::JointTrajec
                    for (int l = 0; l < 6; l++) {
                   jointvaluesOLD[l]=jointvalues[l];
                    }
+
+                                     } //for Points
+
+//                   if (msg.points[0].positions[7]==0.0){
+
+//                     for (int i = 0; i < 6; i++) {
+
+//                       msgpub.points[0].positions[i] = msg.points[0].positions[i];
+//                     }
+
+
+//                     joint_pub_sub.publish(msgpub) ;
+
+//                   }
 
 }
 
